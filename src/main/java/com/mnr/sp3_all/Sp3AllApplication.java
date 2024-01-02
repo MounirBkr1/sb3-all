fd704ec67345f9507c2b9047317325ef544697bf
MounirBkr1 <miirbri1@gmail.com>
2024-01-02 00:21:37 -0500

    first commit

---
+++ src/main/java/com/mnr/sp3_all/Sp3AllApplication.java
@@ -0,0 +1,13 @@
+package com.mnr.sp3_all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp3AllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp3AllApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mnr/sp3_all/Sp3AllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnr.sp3_all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp3AllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
